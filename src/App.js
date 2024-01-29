abead4f523176422862b9e2934dfb8502592ccb6
ZirekSultan <sultanovaaiz17@gmail.com>
2024-01-29 18:33:03 +0600

    dz4m4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AuthForm = () => {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        age: '',
+        email: '',
+        phoneNumber: '',
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        })
+    }
+
+    return (
+        <div>
+            <h2>Форма Регистрации</h2>
+            <form>
+                <label>
+                    ФИО:
+                    <input
+                        type="text"
+                        name="fullName"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Возраст:
+                    <input
+                        type="text"
+                        name="age"
+                        value={formData.age}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Электронная почта:
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    Номер телефона:
+                    <input
+                        type="tel"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+
+            <div>
+                <h2>Данные в режиме REAL-TIME:</h2>
+                <p>ФИО: {formData.fullName}</p>
+                <p>Возраст: {formData.age}</p>
+                <p>Электронная почта: {formData.email}</p>
+                <p>Номер телефона: {formData.phoneNumber}</p>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default AuthForm;
